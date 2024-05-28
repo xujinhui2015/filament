3f6e78215df2e4e91f1727f570231f5a06659899
xujinhui <591196507@qq.com>
2024-05-28 11:01:39 +0800

    update

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -17,6 +17,14 @@ class TestCommand extends Command
 
     public function handle(): void
     {
+        $a = 'page_CustomerResource';
+        $b = 'page_UserResource';
+
+        $aa = User::query()->find(3)->can($a);
+        $bb = User::query()->find(3)->can($b);
+        dump($aa);
+        dump($bb);
+        exit;
         $order = MallOrder::query()->find(30);
         dump($order->delivery_time);
 
