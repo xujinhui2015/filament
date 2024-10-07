7c0df3f7d8b117ab8b978601265cb9352f74f9f0
xujinhui <591196597@qq.com>
2024-10-07 16:35:52 +0800

    feat:增加权限标题的文案

--- app/Filament/Clusters/Mall/MallGoodsCluster.php
+++ app/Filament/Clusters/Mall/MallGoodsCluster.php
@@ -9,7 +9,7 @@ class MallGoodsCluster extends Cluster
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
 
     protected static ?string $navigationLabel = '业务配置';
-
+    protected static ?string $title = '业务配置';
     protected static ?string $navigationGroup = '商城';
 
     public static function shouldRegisterNavigation(): bool
