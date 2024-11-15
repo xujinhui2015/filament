1da82d20f1d3041025bf2eb3af40195fe599598c
xujinhui <591196507@qq.com>
2024-11-15 16:40:00 +0800

    update

--- routes/develop.php
+++ routes/develop.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Develop\TestController;
 use Illuminate\Support\Facades\Route;
 
 // 测试接口
-Route::get('test', [TestController::class, 'index']);
+Route::any('test', [TestController::class, 'index']);
