30cda01ebf2588e4fa75308b6ae8b83d91c6e3d1
xujinhui <591196507@qq.com>
2024-11-25 15:23:29 +0800

    update

--- app/Services/Filament/FilamentShieldService.php
+++ app/Services/Filament/FilamentShieldService.php
@@ -2,57 +2,38 @@
 
 namespace App\Services\Filament;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use RegexIterator;
-
 class FilamentShieldService
 {
-    /**
-     * 获取排除文件
-     */
     public static function getExcludes(): array
     {
         $filterExtends = array_filter(config('extend.custom'), fn($config) => !$config['enabled']);
-
-        // 所有模块都已开启
-        if (!$filterExtends) {
+        if (empty($filterExtends)) {
             return [[], [], []];
         }
 
-        // 拿到开启的模块并且设置首字母为大写
         $excludes = implode(',', array_map(
             fn($panelName) => ucfirst($panelName),
             array_keys($filterExtends)
         ));
-
         $globPath = app_path('Filament') . '/{' . $excludes . '}/';
 
-        // 获取排除资源文件
-        $excludeResources = array_map(
-            function ($resource) {
-                return pathinfo($resource, PATHINFO_FILENAME);
-            },
-            glob($globPath . '{Resources,Clusters}{,/*,/*/*}/*Resource.php', GLOB_BRACE)
-        );
+        // 一次性获取所有文件，并按文件类型分类
+        $allFiles = glob($globPath . '{Resources,Clusters,Widgets}{,/*,/*/*}/*.php', GLOB_BRACE);
 
+        $excludeResources = [];
+        $excludePages = [];
+        $excludeWidgets = [];
 
-        // 获取排除页面文件
-        $excludePages = array_map(
-            function ($resource) {
-                return pathinfo($resource, PATHINFO_FILENAME);
-            },
-            glob($globPath . '{Clusters}{,/*,/*/*}/*Cluster.php', GLOB_BRACE
-            ));
-
-        // 获取排除小组件文件
-        $excludeWidgets = array_map(
-            function ($resource) {
-                return pathinfo($resource, PATHINFO_FILENAME);
-            }, glob(
-                $globPath . '{' . $excludes . '}/{Widgets}{,/*,/*/*}/*Cluster.php', GLOB_BRACE
-            )
-        );
+        foreach ($allFiles as $file) {
+            $filename = pathinfo($file, PATHINFO_FILENAME);
+            if (str_ends_with($file, 'Resource.php')) {
+                $excludeResources[] = $filename;
+            } elseif (str_ends_with($file, 'Cluster.php')) {
+                $excludePages[] = $filename;
+            } elseif (str_contains($file, 'Widgets')) {
+                $excludeWidgets[] = $filename;
+            }
+        }
 
         return [$excludeResources, $excludePages, $excludeWidgets];
     }
